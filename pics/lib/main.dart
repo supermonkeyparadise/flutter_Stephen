6b3e2ca8bd98d3881de0c2e836255b59b0bb054b
Unknown <steven.chou@uniqestate.com>
2018-07-18 11:22:05 +0800

    StatelessWidget

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -2,24 +2,11 @@
 // from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
+import 'src/app.dart';
+
 // Define a 'main' function to run when our app starts
 void main() {
-  // Create a new text widget to show some text
-  // on the screen
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-      appBar: AppBar(
-        title: Text('Let\'s see some images!'),
-      ),
-    ),
-  );
-
   // Take that widget and get it on the screen
-  runApp(app);
+  // take our app class creates a new instance out of it
+  runApp(App());
 }
