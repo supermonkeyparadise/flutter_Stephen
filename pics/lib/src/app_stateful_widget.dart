b11968f2331c0918fc4f88b63aa7757d93febac2
Unknown <steven.chou@uniqestate.com>
2018-07-20 14:48:57 +0800

    Updating the AppState Widget

--- pics/lib/src/app_stateful_widget.dart
+++ pics/lib/src/app_stateful_widget.dart
@@ -18,12 +18,17 @@ class App extends StatefulWidget {
 // widget state class
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     counter++;
     var response =
         await get('https://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   // Must define a 'build' method that returns
