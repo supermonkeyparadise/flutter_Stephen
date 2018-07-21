a706e53f5bd34ac3cd0cd73cb60c7f8a2735a300
Unknown <steven.chou@uniqestate.com>
2018-07-21 17:02:35 +0800

    Selective Padding

--- pics/lib/src/widgets/imag_list.dart
+++ pics/lib/src/widgets/imag_list.dart
@@ -25,7 +25,13 @@ class ImageList extends StatelessWidget {
       padding: EdgeInsets.all(20.0),
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: <Widget>[Image.network(image.url), Text(image.title)],
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 8.0),
+          ),
+          Text(image.title)
+        ],
       ),
     );
   }
