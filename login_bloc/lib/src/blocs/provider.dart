31e0b1db453294986b64eb24e2c60e3ac07ff040
Unknown <steven.chou@uniqestate.com>
2018-07-31 17:56:13 +0800

    The Provider's Constructor

--- login_bloc/lib/src/blocs/provider.dart
+++ login_bloc/lib/src/blocs/provider.dart
@@ -6,6 +6,8 @@ import './bloc.dart';
 class Provider extends InheritedWidget {
   final bloc = Bloc();
 
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
 
