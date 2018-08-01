e7ebf6d18070521520e400a037b5196aa3d82302
Unknown <steven.chou@uniqestate.com>
2018-08-01 16:10:23 +0800

    Accessing the Bloc 補充註解

--- login_bloc/lib/src/blocs/provider.dart
+++ login_bloc/lib/src/blocs/provider.dart
@@ -14,6 +14,7 @@ class Provider extends InheritedWidget {
   static Bloc of(BuildContext context) {
     // 1. look up the tree find Provider type
     // 2. as Provider ===> 表示回傳的 widget 必須是一個 Provider 的實體
+    // 3. ＊＊ 使用 as Provider 之後，才可以使用 Provider 內的成員！！ ＊＊
     return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
   }
 }
